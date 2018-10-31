e0c3b5fbdb2b298e215403af1e15eb1a275ccf8d
Theron Tjapkes <theron@zmagnet.com>
2018-10-31 14:20:45 -0400

    Lab09 up to exercise 9.2 part 4

---
+++ lab09/webpack.config.js
@@ -0,0 +1,21 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: [
+        __dirname + '/app/scripts/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: '/bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.css$/,  loader: 'style!css?modules!postcss' }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"})
+        ]
+    
+};
