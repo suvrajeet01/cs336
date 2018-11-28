662737077a3bd3c5a61632a25156ecfa2e970a33
Theron Tjapkes <theron@zmagnet.com>
2018-11-28 15:53:11 -0500

    Start of Lab11

---
+++ lab13/app/scripts/comment-list.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Comment from './comment';
+
+module.exports = React.createClass({
+    render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
